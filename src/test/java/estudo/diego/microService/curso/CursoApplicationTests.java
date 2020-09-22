0ada553cc36fd10c60962c3b8c1ea3957042015d
Diego Mota <diegomota@hpeautos.com.br>
2020-09-22 18:10:30 -0300

    Primeiro Commit

---
+++ src/test/java/estudo/diego/microService/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.diego.microService.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
